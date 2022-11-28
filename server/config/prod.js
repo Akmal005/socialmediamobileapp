f5392d0d5c42e8ef938149ddec9a76c3f9e59ab6
Akmal005 <akmalazizkulov@gmail.com>
2022-11-28 14:18:19 -0500

    Changes

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,3 @@
 module.exports={
-    MongoDB:'mongodb+srv://akmal2005:<k6PSnivG1UGeCSc7>@mywebsite.sc9bh.mongodb.net/testongodb+srv://quick_chat:k6PSnivG1UGeCSc7@mongodb-database.krlyb.mongodb.net/?retryWrites=true&w=majority'
+    MongoDB:process.env.MongoDB
 }
